4b0de669548d3574daccf12a4be41a08994aef9b
lucasberenger <thegreatberenger@gmail.com>
2025-01-30 22:40:14 -0300

    create error exceptions to findUserById

--- src/main/java/com/webservice/Shop/services/UserService.java
+++ src/main/java/com/webservice/Shop/services/UserService.java
@@ -2,6 +2,7 @@ package com.webservice.Shop.services;
 
 import com.webservice.Shop.entities.User;
 import com.webservice.Shop.repositories.UserRepository;
+import com.webservice.Shop.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {

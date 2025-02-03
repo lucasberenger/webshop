6c1f300e6a4bc6855c0e892829a132cf8102d218
lucasberenger <thegreatberenger@gmail.com>
2025-02-03 10:39:26 -0300

    create Exception Handling for http put request

--- src/main/java/com/webservice/Shop/services/UserService.java
+++ src/main/java/com/webservice/Shop/services/UserService.java
@@ -4,6 +4,7 @@ import com.webservice.Shop.entities.User;
 import com.webservice.Shop.repositories.UserRepository;
 import com.webservice.Shop.services.exceptions.DataBaseException;
 import com.webservice.Shop.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
